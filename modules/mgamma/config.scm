8c30f398786cdecea07b1cfb45dbc44c556df1ef
Artyom Bologov <mail@aartaka.me>
2024-07-18 02:05:54 +0400

    config: Add mvlmm params.

--- modules/mgamma/config.scm
+++ modules/mgamma/config.scm
@@ -13,6 +13,14 @@
 (define l-mle-null (make-parameter 0))
 (define log-mle-null (make-parameter 0))
 
+;; mvLMM params
+(define p-nr (make-parameter 0.001))
+(define em-iter (make-parameter 10000))
+(define em-precision (make-parameter 0.0001))
+(define nr-iter (make-parameter 100))
+(define nr-precision (make-parameter 0.0001))
+(define crt (make-parameter 0))
+
 ;; Defining π here (as the largest precision fraction from Wikipedia),
 ;; because I haven't found it in Guile standard lib (at least in
 ;; the manual).
