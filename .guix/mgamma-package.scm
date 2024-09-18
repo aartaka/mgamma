12a8ffb11e0f15d888e3e7b922ebfa900dbab390
Artyom Bologov <mail@aartaka.me>
2024-09-18 14:55:44 +0400

    .guix/mgamma-package: Make it build with the extension.

--- .guix/mgamma-package.scm
+++ .guix/mgamma-package.scm
@@ -1,18 +1,21 @@
 (define-module (mgamma-package)
- #:use-module (gnu packages guile)
- #:use-module (gnu packages guile-xyz)
- #:use-module (gnu packages databases)
- #:use-module (gnu packages bash)
- #:use-module (gnu packages maths)
- #:use-module (guix packages)
- #:use-module (guix gexp)
- #:use-module (guix utils)
- #:use-module (guix build-system guile)
- #:use-module (guix git-download)
- #:use-module ((guix licenses) #:prefix license:)
- #:use-module (guile-gsl-package)
- #:use-module (guile-lmdb-package)
- #:use-module (guile-lapack-package))
+  #:use-module (guix build-system guile)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages commencement)
+  #:use-module (gnu packages databases)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages guile-xyz)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (guile-gsl-package)
+  #:use-module (guile-lmdb-package)
+  #:use-module (guile-lapack-package))
 
 (define-public mgamma-git
   (package
@@ -44,29 +47,52 @@
                         (("libopenblas.so")
                          openblas))
                        #t)))
+                  (add-before 'build 'build-libmgamma
+                    (lambda* (#:key inputs outputs #:allow-other-keys)
+                      (let ((extdir (string-append (assoc-ref outputs "out")
+                                                   "/lib/guile/3.0/extensions")))
+                        (invoke "make" "-C" "extension"
+                                (string-append "CC="
+                                               (assoc-ref inputs "gcc-toolchain")
+                                               "/bin/gcc")
+                                "libmgamma.so")
+                        (mkdir-p extdir)
+                        (copy-file "extension/libmgamma.so"
+                                   (string-append extdir "/libmgamma.so")))))
+                  (add-before 'build 'substitute-libmgamma
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      (substitute*
+                          '("modules/mgamma/utils.scm")
+                        (("load-extension \"libmgamma.so\"")
+                         ;; Bump the version when updating
+                         (format #f "load-extension \"~a/lib/guile/3.0/extensions/libmgamma.so\""
+                                 (assoc-ref outputs "out"))))))
                   (add-after 'build 'make-bin
-                   (lambda* (#:key inputs outputs #:allow-other-keys)
-                     (let* ((bin (string-append (assoc-ref outputs "out")
-                                                "/bin"))
-                            (real (string-append bin "/mgamma"))
-                            (bash (string-append (assoc-ref inputs "bash")
-                                                 "/bin/bash"))
-                            (self (string-append (assoc-ref outputs "out")
-                                                 "/share/guile/site/3.0")))
-                       (mkdir-p bin)
-                       (copy-file "bin/mgamma" real)
-                       (wrap-program real #:sh bash
-                                     ;; For better backtraces.
-                                     (list "COLUMNS" ":" '= (list "1000"))
-                                     (list "GUILE_LOAD_PATH" ":" '= (list self (getenv "GUILE_LOAD_PATH"))))))))))
+                    (lambda* (#:key inputs outputs #:allow-other-keys)
+                      (let* ((bin (string-append (assoc-ref outputs "out")
+                                                 "/bin"))
+                             (real (string-append bin "/mgamma"))
+                             (bash (string-append (assoc-ref inputs "bash")
+                                                  "/bin/bash"))
+                             (self (string-append (assoc-ref outputs "out")
+                                                  "/share/guile/site/3.0")))
+                        (mkdir-p bin)
+                        (copy-file "bin/mgamma" real)
+                        (wrap-program real #:sh bash
+                                      ;; For better backtraces.
+                                      (list "COLUMNS" ":" '= (list "1000"))
+                                      (list "GUILE_LOAD_PATH" ":" '= (list self (getenv "GUILE_LOAD_PATH"))))))))))
     (native-inputs (list guile-3.0))
     (inputs (list bash
+                  gnu-make
                   guile-3.0
+                  gcc-toolchain
                   openblas
                   guile-json-4
                   guile-lmdb-git
                   guile-gsl-git
-                  guile-lapack-git))
+                  guile-lapack-git
+                  pkg-config))
     (home-page "https://github.com/aartaka/guile-gemma")
     (synopsis "Mgamma is a tool for Genome-Wide Association Studies (GWAS).")
     (description "This package provides a Guile @code{(mgamma core)} module and a
